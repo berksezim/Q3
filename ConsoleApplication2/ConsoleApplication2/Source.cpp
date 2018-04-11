b3a8aacb17d9f6537e9b99640791df4657f4da3e
21503108 <21503108@LAB112>
2018-04-11 15:58:11 +0300

    asffsasfafsasfa

---
+++ ConsoleApplication2/ConsoleApplication2/Source.cpp
@@ -0,0 +1,9 @@
+int sumOfDigits(int num) { 
+
+	int main(void) {
+		
+		int num;
+	
+		
+		
+		printf("Enter a num: "); scanf("%d", &num);  printf("The sum of the num %d's digits is %d.\n", num, sumOfDigits(num));
